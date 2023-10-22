1933ecf3a4bed8656cf20f3c4ae4b38f995f814e
Mazhar Kartal <Mazhar Kartal@DESKTOP-LKN23GJ>
2023-10-23 00:37:10 +0300

    Proje dosyası ekle.

---
+++ personelbilgileri/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace personelbilgileri
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtno = new System.Windows.Forms.TextBox();
+            this.txtad = new System.Windows.Forms.TextBox();
+            this.txtsoyad = new System.Windows.Forms.TextBox();
+            this.txtmaas = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnekle = new System.Windows.Forms.Button();
+            this.btngncl = new System.Windows.Forms.Button();
+            this.btnsıl = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.txtarama = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.personelno = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.personelad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.personelsoyad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gırıstarıhı = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.maas = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(29, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "numarası";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(29, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "adı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(29, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "soyadı";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(16, 257);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "giriş tarihi";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(29, 219);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "maaşı";
+            // 
+            // txtno
+            // 
+            this.txtno.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtno.Location = new System.Drawing.Point(113, 26);
+            this.txtno.Name = "txtno";
+            this.txtno.Size = new System.Drawing.Size(100, 26);
+            this.txtno.TabIndex = 5;
+            // 
+            // txtad
+            // 
+            this.txtad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtad.Location = new System.Drawing.Point(113, 81);
+            this.txtad.Name = "txtad";
+            this.txtad.Size = new System.Drawing.Size(100, 26);
+            this.txtad.TabIndex = 6;
+            // 
+            // txtsoyad
+            // 
+            this.txtsoyad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtsoyad.Location = new System.Drawing.Point(113, 127);
+            this.txtsoyad.Name = "txtsoyad";
+            this.txtsoyad.Size = new System.Drawing.Size(100, 26);
+            this.txtsoyad.TabIndex = 7;
+            // 
+            // txtmaas
+            // 
+            this.txtmaas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtmaas.Location = new System.Drawing.Point(113, 212);
+            this.txtmaas.Name = "txtmaas";
+            this.txtmaas.Size = new System.Drawing.Size(100, 26);
+            this.txtmaas.TabIndex = 8;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(113, 257);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(161, 26);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
+            // btnekle
+            // 
+            this.btnekle.BackColor = System.Drawing.Color.Gray;
+            this.btnekle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnekle.Location = new System.Drawing.Point(37, 322);
+            this.btnekle.Name = "btnekle";
+            this.btnekle.Size = new System.Drawing.Size(88, 44);
+            this.btnekle.TabIndex = 10;
+            this.btnekle.Text = "Ekle";
+            this.btnekle.UseVisualStyleBackColor = false;
+            this.btnekle.Click += new System.EventHandler(this.btnekle_Click);
+            // 
+            // btngncl
+            // 
+            this.btngncl.BackColor = System.Drawing.Color.Gray;
+            this.btngncl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btngncl.Location = new System.Drawing.Point(173, 322);
+            this.btngncl.Name = "btngncl";
+            this.btngncl.Size = new System.Drawing.Size(88, 44);
+            this.btngncl.TabIndex = 11;
+            this.btngncl.Text = "Güncelle";
+            this.btngncl.UseVisualStyleBackColor = false;
+            // 
+            // btnsıl
+            // 
+            this.btnsıl.BackColor = System.Drawing.Color.Gray;
+            this.btnsıl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnsıl.Location = new System.Drawing.Point(317, 321);
+            this.btnsıl.Name = "btnsıl";
+            this.btnsıl.Size = new System.Drawing.Size(88, 44);
+            this.btnsıl.TabIndex = 12;
+            this.btnsıl.Text = "Sil";
+            this.btnsıl.UseVisualStyleBackColor = false;
+            this.btnsıl.Click += new System.EventHandler(this.btnsıl_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Gray;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(647, 34);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 44);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Arama";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Gray;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(438, 321);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(140, 44);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Kayıtları listele";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // txtarama
+            // 
+            this.txtarama.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtarama.Location = new System.Drawing.Point(489, 52);
+            this.txtarama.Name = "txtarama";
+            this.txtarama.Size = new System.Drawing.Size(152, 26);
+            this.txtarama.TabIndex = 15;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.personelno,
+            this.personelad,
+            this.personelsoyad,
+            this.gırıstarıhı,
+            this.maas});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 436);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(888, 281);
+            this.dataGridView1.TabIndex = 16;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // personelno
+            // 
+            this.personelno.DataPropertyName = "personelno";
+            this.personelno.HeaderText = "numara";
+            this.personelno.Name = "personelno";
+            // 
+            // personelad
+            // 
+            this.personelad.DataPropertyName = "personelad";
+            this.personelad.HeaderText = "adı";
+            this.personelad.Name = "personelad";
+            // 
+            // personelsoyad
+            // 
+            this.personelsoyad.DataPropertyName = "personelsoyad";
+            this.personelsoyad.HeaderText = "soyadı";
+            this.personelsoyad.Name = "personelsoyad";
+            // 
+            // gırıstarıhı
+            // 
+            this.gırıstarıhı.DataPropertyName = "gırıstarıhı";
+            this.gırıstarıhı.HeaderText = "işe giriş tarihi";
+            this.gırıstarıhı.Name = "gırıstarıhı";
+            // 
+            // maas
+            // 
+            this.maas.DataPropertyName = "maas";
+            this.maas.HeaderText = "maaşı";
+            this.maas.Name = "maas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(912, 719);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtarama);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnsıl);
+            this.Controls.Add(this.btngncl);
+            this.Controls.Add(this.btnekle);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtmaas);
+            this.Controls.Add(this.txtsoyad);
+            this.Controls.Add(this.txtad);
+            this.Controls.Add(this.txtno);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtno;
+        private System.Windows.Forms.TextBox txtad;
+        private System.Windows.Forms.TextBox txtsoyad;
+        private System.Windows.Forms.TextBox txtmaas;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnekle;
+        private System.Windows.Forms.Button btngncl;
+        private System.Windows.Forms.Button btnsıl;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox txtarama;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn personelno;
+        private System.Windows.Forms.DataGridViewTextBoxColumn personelad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn personelsoyad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn gırıstarıhı;
+        private System.Windows.Forms.DataGridViewTextBoxColumn maas;
+    }
+}
+
---
+++ personelbilgileri/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace personelbilgileri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        SqlConnection bağlantı= new SqlConnection("Data Source=DESKTOP-LKN23GJ\\SQLEXPRESS;Initial Catalog=personeltakip;Integrated Security=True");
+        
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("select *from Personelbilgi", bağlantı);
+            DataSet ds =new DataSet();
+            da.Fill(ds,"sanaltablo");
+            dataGridView1.DataSource=ds.Tables["sanaltablo"];
+
+        }
+
+        private void btnekle_Click(object sender, EventArgs e)
+        {
+            bağlantı.Open();
+            SqlCommand komut = new SqlCommand("insert into Personelbilgi(personelno,personelad,personelsoyad,gırıstarıhı,maas) values(@no,@ad,@soyad,@tarih,@maas)", bağlantı);
+            komut.Parameters.AddWithValue("@no",txtno.Text);
+            komut.Parameters.AddWithValue("@ad",txtad.Text);   
+            komut.Parameters.AddWithValue("@soyad",txtsoyad.Text);
+            komut.Parameters.AddWithValue("@tarih", dateTimePicker1.Value);
+            komut.Parameters.AddWithValue("@maas", txtmaas.Text);
+            komut.ExecuteNonQuery();
+            bağlantı.Close();
+             Temizle();
+               
+          
+
+        }
+       void Temizle()
+        {
+            txtno.Text = "";
+            txtad.Text = "";
+            txtsoyad.Text = "";
+            dateTimePicker1.Value = DateTime.Now;
+            txtmaas.Text = "";
+            txtarama.Text = "";
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtno.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            txtad.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txtsoyad.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            dateTimePicker1.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            txtmaas.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+        }
+
+        private void btnsıl_Click(object sender, EventArgs e)
+        {
+            DialogResult sonuc = new DialogResult();
+            sonuc = MessageBox.Show("Bu kaydı silmek istediğinize eminmisiniz", "Silme işlemi", MessageBoxButtons.YesNoCancel);
+            if (sonuc == DialogResult.Yes)
+            {
+                bağlantı.Open();
+                SqlCommand komut = new SqlCommand("Delete from Personelbilgi where personelno=@no", bağlantı);
+                komut.Parameters.AddWithValue("@no",txtno.Text);
+                komut.ExecuteNonQuery();
+                bağlantı.Close();
+                Temizle();
+
+            }
+        }
+    }
+}
